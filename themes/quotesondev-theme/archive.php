3d55f453def32b966c84612c784b43d52cdfe34a
Kachniss <vopalkova.katerina@gmail.com>
2018-05-26 14:50:40 -0700

    Edit template for categories&tags, add post meta data

--- themes/quotesondev-theme/archive.php
+++ themes/quotesondev-theme/archive.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main archives" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -27,7 +27,10 @@ get_header(); ?>
 
 			<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
+			<?php the_posts_pagination( array(
+				'prev_text' => __( '&larr; Previous', 'textdomain' ),
+				'next_text' => __( 'Next &rarr;', 'textdomain' ),
+			) ); ?>
 
 		<?php else : ?>
 
--- themes/quotesondev-theme/single.php
+++ themes/quotesondev-theme/single.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main home-page" role="main">
+		<main id="main" class="site-main single-quote" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
